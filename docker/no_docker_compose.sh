cca2f7e37501dafa3b5cc4523296af1b4358b253
Matt Shubat <mattshubat@gmail.com>
2023-07-30 16:29:51 -0400

    two services on the  same network

--- docker/no_docker_compose.sh
+++ docker/no_docker_compose.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 
-# Set the name of the docker image, container and zip file for website
-IMAGE_NAME="devops-website-image"
-CONTAINER_NAME="devops-website-container"
 WEBSITE_ZIP_FILE="car-repair-html-template.zip"
 
+WEB_IMAGE_NAME="devops-website-image"
+WEB_CONTAINER_NAME="devops-website-container"
+DB_CONTAINER_NAME="mock-mysql"
+
+
 # Unzip the webpage of choice
-unzip $WEBSITE_ZIP_FILE
+unzip -qq $WEBSITE_ZIP_FILE
 
 # Start a container from the base image
-docker run --name $CONTAINER_NAME -d -p 8080:80 nginx:alpine
+docker run --name $WEB_CONTAINER_NAME -d -p 8080:80 nginx:alpine
 
 # Copy the website files into the container
-docker cp ./car-repair-html-template/. $CONTAINER_NAME:/usr/share/nginx/html
+docker cp ./car-repair-html-template/. $WEB_CONTAINER_NAME:/usr/share/nginx/html
 
 # Commit the container's state to a new image
-docker commit $CONTAINER_NAME $IMAGE_NAME
+docker commit $WEB_CONTAINER_NAME $WEB_IMAGE_NAME
 
 # Notify user
-echo "Docker image $IMAGE_NAME has been created and is running on http://localhost:8080"
+echo "Docker image $WEB_IMAGE_NAME has been created and is running on http://localhost:8080"
+
+# Setup the MySQL container
+echo "Creating the MySQL container..."
+docker run --name $DB_CONTAINER_NAME -e MYSQL_ROOT_PASSWORD=fake_password -d mysql:latest
+
+echo "Settings up the network..."
+
+if [ ! "$(docker network ls | grep my-network)" ]; then
+  echo "Creating my-network..."
+  docker network create my-network
+else
+  echo "my-network exists."
+fi
+
+docker network connect my-network $WEB_CONTAINER_NAME
+docker network connect my-network $DB_CONTAINER_NAME
+
+
